8e2e2fe74ee38099bf990a764c490f05c5dc1bfc
dahomeykid <jacksovoessi@hotmail.com>
2022-02-03 10:00:29 +0100

    Add valiation on POST & PUT ContactController

--- src/main/java/com/example/contactapp/contact/ContactController.java
+++ src/main/java/com/example/contactapp/contact/ContactController.java
@@ -3,13 +3,20 @@ package com.example.contactapp.contact;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+@CrossOrigin(origins = "https://contact-app-v6.herokuapp.com")
 @RestController
 @RequestMapping("api/v1/contacts")
 @AllArgsConstructor
@@ -29,7 +36,7 @@ public class ContactController {
     }
 
     @PostMapping
-    public ResponseEntity<Contact> createContact(@RequestBody Contact contact){
+    public ResponseEntity<Contact> createContact(@Valid @RequestBody Contact contact){
         Contact created = contactService.saveContact(contact);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{contactId}")
@@ -46,7 +53,7 @@ public class ContactController {
 
     @PutMapping("/{contactId}")
     public ResponseEntity<Contact> updateContact(
-            @PathVariable("contactId") String id,
+            @Valid @PathVariable("contactId") String id,
             @RequestBody Contact updatedContact){
 
         Optional<Contact> updated = contactService.updateContact(id, updatedContact);
@@ -68,5 +75,17 @@ public class ContactController {
     public ResponseEntity<Contact> delete(@PathVariable("contactId") String id) {
         contactService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<ObjectError> errors = ex.getBindingResult().getAllErrors();
+        Map<String, String> map = new HashMap<>(errors.size());
+        errors.forEach((error) -> {
+            String key = ((FieldError) error).getField();
+            String val = error.getDefaultMessage();
+            map.put(key, val);
+        });
+        return ResponseEntity.badRequest().body(map);
     }
 }
